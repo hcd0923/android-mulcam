1a86cfaa2ee260df6f7421c76288029ada92735e
cd <hcd0923@gmail.com>
2019-12-17 16:56:54 +0900

    6장 예제 DBMS 추가 중 1

---
+++ pjt_student/src/main/java/com/example/pjt_student/DBHelper.java
@@ -0,0 +1,39 @@
+package com.example.pjt_student;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "studentdb", null, 1);
+    }
+
+//  AppInstall 후  어디선가 최초로 이용되는 순간 호출
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String studentSql = "create table tb_student (" +
+                "_id integer primary key autoincement," +
+                "name not null," +
+                "email," +
+                "phone," +
+                "photo," +
+                "memo";
+
+        String scoreSql = "create table tb_score (" +
+                "_id integer primary key autoincement," +
+                "student_id not null," +
+                "date," +
+                "score";
+
+        db.execSQL(studentSql);
+        db.execSQL(scoreSql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table tb_student");
+        db.execSQL("drop table tb_score");
+        onCreate(db);
+    }
+}
